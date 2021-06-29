dccbd01f6937b834fe8898f7a9e13bfad9f05135
Kritika <24kritikasharma24@gmail.com>
2021-06-29 23:31:29 +0530

    setup routes for different pages with their urls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Switch } from 'react-router-dom'
 
+import AllMeetupsPage from './pages/AllMeetups'
+import NewMeetupPage from './pages/NewMeetup'
+import FavoritesPage from './pages/Favorites'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Switch>
+        <Route path='/' exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
   )
 }
 
-export default App;
+export default App
