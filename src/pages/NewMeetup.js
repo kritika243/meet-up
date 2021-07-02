adca0028afa71ea0928ee6bd8d443d0c4ba277da
Kritika <24kritikasharma24@gmail.com>
2021-07-02 16:05:05 +0530

    sending the POST requst to firebase for form submission

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,18 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
-  function addMeetupHandler(formData) {}
+  function addMeetupHandler(formData) {
+    fetch(
+      'https://react-meet-up-1d80b-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
